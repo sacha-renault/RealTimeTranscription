d47445c22b33ad620aa8f6f06fb870776333f93b
sacha-renault <sacha35340@gmail.com>
2024-12-27 22:13:09 +0100

    fix: add Pinia to main application for state management

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import naive from 'naive-ui'
+import { createPinia } from 'pinia'
 
 const app = createApp(App);
 app.use(naive);
+app.use(createPinia());
 app.mount("#app");
